95700e6cf121243d64af7cfc2ca74021e484655c
Michal Konturek <michal.konturek@gmail.com>
2014-04-16 23:31:02 +0100

    Added BarGraphView.

---
+++ Source/BarGraph/BarGraphView.h
@@ -0,0 +1,13 @@
+//
+//  BarGraphView.h
+//  GraphKit
+//
+//  Created by Michal Konturek on 16/04/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BarGraphView : UIView
+
+@end
---
+++ Source/BarGraph/BarGraphView.m
@@ -0,0 +1,31 @@
+//
+//  BarGraphView.m
+//  GraphKit
+//
+//  Created by Michal Konturek on 16/04/2014.
+//  Copyright (c) 2014 Michal Konturek. All rights reserved.
+//
+
+#import "BarGraphView.h"
+
+@implementation BarGraphView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
